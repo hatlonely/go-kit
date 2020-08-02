2b990f5e7d8d5a1e36ee26957125514a310e4592
hatlonely <hatlonely@gmail.com>
2020-08-03 00:44:28 +0800

    update

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -15,6 +16,30 @@ func TestLogger(t *testing.T) {
 		"hello": "world",
 		"key1":  "key2",
 	})
+}
+
+func TestParallel(t *testing.T) {
+	w, err := NewRotateFileWriter("hello.info", 24*time.Hour)
+	if err != nil {
+		panic(err)
+	}
+	l := NewLogger(0, w)
+
+	var wg sync.WaitGroup
+	start := time.Now()
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			for time.Now().Before(start.Add(10 * time.Second)) {
+				l.Info(map[string]string{
+					"hello": "world",
+					"key1":  "key2",
+				})
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 func BenchmarkHello(b *testing.B) {
