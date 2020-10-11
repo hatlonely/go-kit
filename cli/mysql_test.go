74e4a14cc6c9e09225adfcd8847e2c29f137b13b
hatlonely <hatlonely@gmail.com>
2020-10-11 21:51:27 +0800

    update

---
+++ cli/mysql_test.go
@@ -0,0 +1,32 @@
+package cli
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/hatlonely/go-kit/config"
+)
+
+func TestNewMysqlWithConfig(t *testing.T) {
+	Convey("TestNewMysqlWithConfig", t, func() {
+		So(ioutil.WriteFile("1.json", []byte(`{
+		  "username": "root",
+		  "password": "",
+		  "database": "account",
+		  "host": "127.0.0.1",
+		  "port": 3306,
+		  "connMaxLifeTime": "40s",
+		  "maxIdleConns": 12,
+		  "maxOpenConns": 23
+		}`), 0644), ShouldBeNil)
+		cfg, err := config.NewSimpleFileConfig("1.json")
+		So(err, ShouldBeNil)
+		client, err := NewMysqlWithConfig(cfg)
+		So(err, ShouldBeNil)
+		So(client, ShouldNotBeNil)
+		So(os.RemoveAll("1.json"), ShouldBeNil)
+	})
+}
