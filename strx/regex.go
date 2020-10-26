cdec99952fd7943f70f52163c0303ac249582832
realwrtoff <fmj_912@163.com>
2020-10-26 13:30:28 +0800

    Update regex.go

--- strx/regex.go
+++ strx/regex.go
@@ -5,3 +5,4 @@ import "regexp"
 var ReIdentifier = regexp.MustCompile(`^[a-zA-Z]\w+$`)
 var ReEmail = regexp.MustCompile(`^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$`)
 var RePhone = regexp.MustCompile(`^1[345789][0-9]{9}$`)
+var ReUserName = regexp.MustCompile(`^[a-zA-Z0-9_]\w+$`)
