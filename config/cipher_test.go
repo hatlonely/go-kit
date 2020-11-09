22657f553710a69564a6bed23db568806f84f66e
hatlonely <hatlonely@gmail.com>
2020-11-09 23:57:11 +0800

    update

--- config/cipher_test.go
+++ config/cipher_test.go
@@ -122,3 +122,54 @@ func TestKMSCipher(t *testing.T) {
 
 	})
 }
+
+func TestAESCipher(t *testing.T) {
+	Convey("TestAESCipher 1", t, func() {
+		cipher, err := NewCipherWithOptions(&CipherOptions{
+			Type: "AES",
+			AESCipher: AESCipherOptions{
+				Key: "123456",
+			},
+		})
+		So(err, ShouldBeNil)
+
+		buf, err := cipher.Encrypt([]byte("hello world"))
+		So(err, ShouldBeNil)
+		buf, err = cipher.Decrypt(buf)
+		So(err, ShouldBeNil)
+		So(string(buf), ShouldEqual, "hello world")
+	})
+
+	Convey("TestAESCipher 2", t, func() {
+		patches := ApplyMethod(reflect.TypeOf(&kms.Client{}), "Decrypt", func(
+			cli *kms.Client, request *kms.DecryptRequest) (*kms.DecryptResponse, error) {
+			return &kms.DecryptResponse{
+				Plaintext: "nV+j39GExjRZCX2kUkYB/gDMTE9F7H15bZ3faddUMB4=",
+			}, nil
+		})
+		defer patches.Reset()
+
+		cipher, err := NewCipherWithOptions(&CipherOptions{
+			Type: "AES",
+			AESCipher: AESCipherOptions{
+				KMSKey: "NWMzYmNjODQtNTgxMC00NGZmLTkwMTAtNWIwMGY1NzhiNTg129Uj83I4hoqFOFsKrx/SSiuSn+zOHr/vUVdi8t7z1Bw/swRjHwE5NoBV6wn8RMG5rM1pvgg70bZwEYjUHdzP+NS+AgiWmy/t",
+				KMS: struct {
+					AccessKeyID     string
+					AccessKeySecret string
+					RegionID        string
+				}{
+					AccessKeyID:     "xx",
+					AccessKeySecret: "xx",
+					RegionID:        "cn-shanghai",
+				},
+			},
+		})
+		So(err, ShouldBeNil)
+
+		buf, err := cipher.Encrypt([]byte("hello world"))
+		So(err, ShouldBeNil)
+		buf, err = cipher.Decrypt(buf)
+		So(err, ShouldBeNil)
+		So(string(buf), ShouldEqual, "hello world")
+	})
+}
