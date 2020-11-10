b96b3121d140f27825ad8736ad7abfdbe9387bdc
hatlonely <hatlonely@gmail.com>
2020-11-11 01:08:43 +0800

    update

--- config/autogen_global_export_test.go
+++ config/autogen_global_export_test.go
@@ -282,3 +282,124 @@ func TestGlobalBinds(t *testing.T) {
 		DeleteTestFile()
 	})
 }
+
+func TestGlobalBindVars(t *testing.T) {
+	Convey("TestBindVars", t, func(c C) {
+		CreateTestFile2()
+
+		var boolVal AtomicBool
+		var intVal AtomicInt
+		var int8Val AtomicInt8
+		var int16Val AtomicInt16
+		var int32Val AtomicInt32
+		var int64Val AtomicInt64
+		var uintVal AtomicUint
+		var uint8Val AtomicUint8
+		var uint16Val AtomicUint16
+		var uint32Val AtomicUint32
+		var uint64Val AtomicUint64
+		var float32Val AtomicFloat32
+		var float64Val AtomicFloat64
+		var stringVal AtomicString
+		var durationVal AtomicDuration
+		var timeVal AtomicTime
+		var ipVal AtomicIP
+		err := InitWithSimpleFile("test.json")
+		So(err, ShouldBeNil)
+		So(Watch(), ShouldBeNil)
+		defer Stop()
+
+		BoolVar("boolVal", &boolVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetBool(""), ShouldEqual, true)
+		}))
+		IntVar("intVal", &intVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetInt(""), ShouldEqual, 11)
+		}))
+		Int8Var("int8Val", &int8Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetInt8(""), ShouldEqual, 22)
+		}))
+		Int16Var("int16Val", &int16Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetInt16(""), ShouldEqual, 33)
+		}))
+		Int32Var("int32Val", &int32Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetInt32(""), ShouldEqual, 44)
+		}))
+		Int64Var("int64Val", &int64Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetInt64(""), ShouldEqual, 55)
+		}))
+		UintVar("uintVal", &uintVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetUint(""), ShouldEqual, 66)
+		}))
+		Uint8Var("uint8Val", &uint8Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetUint8(""), ShouldEqual, 77)
+		}))
+		Uint16Var("uint16Val", &uint16Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetUint16(""), ShouldEqual, 88)
+		}))
+		Uint32Var("uint32Val", &uint32Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetUint32(""), ShouldEqual, 99)
+		}))
+		Uint64Var("uint64Val", &uint64Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetUint64(""), ShouldEqual, 1010)
+		}))
+		Float32Var("float32Val", &float32Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetFloat32(""), ShouldBeBetween, 32.31, 32.33)
+		}))
+		Float64Var("float64Val", &float64Val, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetFloat64(""), ShouldBeBetween, 64.63, 64.65)
+		}))
+		StringVar("stringVal", &stringVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetString(""), ShouldEqual, "world")
+		}))
+		DurationVar("durationVal", &durationVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetDuration(""), ShouldEqual, 6*time.Second)
+		}))
+		TimeVar("timeVal", &timeVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetTime(""), ShouldEqual, time.Unix(1604930127, 0))
+		}))
+		IPVar("ipVal", &ipVal, OnSucc(func(cfg *Config) {
+			c.So(cfg.GetIP(""), ShouldEqual, net.ParseIP("106.11.34.43"))
+		}))
+
+		So(boolVal.Get(), ShouldEqual, false)
+		So(intVal.Get(), ShouldEqual, 1)
+		So(int8Val.Get(), ShouldEqual, 2)
+		So(int16Val.Get(), ShouldEqual, 3)
+		So(int32Val.Get(), ShouldEqual, 4)
+		So(int64Val.Get(), ShouldEqual, 5)
+		So(uintVal.Get(), ShouldEqual, 6)
+		So(uint8Val.Get(), ShouldEqual, 7)
+		So(uint16Val.Get(), ShouldEqual, 8)
+		So(uint32Val.Get(), ShouldEqual, 9)
+		So(uint64Val.Get(), ShouldEqual, 10)
+		So(float32Val.Get(), ShouldBeBetween, 3.1, 3.3)
+		So(float64Val.Get(), ShouldBeBetween, 6.3, 6.5)
+		So(stringVal.Get(), ShouldEqual, "hello")
+		So(durationVal.Get(), ShouldEqual, 3*time.Second)
+		So(timeVal.Get(), ShouldEqual, time.Unix(1604930126, 0))
+		So(ipVal.Get(), ShouldEqual, net.ParseIP("106.11.34.42"))
+
+		CreateTestFile3()
+		time.Sleep(time.Second)
+
+		So(boolVal.Get(), ShouldEqual, true)
+		So(intVal.Get(), ShouldEqual, 11)
+		So(int8Val.Get(), ShouldEqual, 22)
+		So(int16Val.Get(), ShouldEqual, 33)
+		So(int32Val.Get(), ShouldEqual, 44)
+		So(int64Val.Get(), ShouldEqual, 55)
+		So(uintVal.Get(), ShouldEqual, 66)
+		So(uint8Val.Get(), ShouldEqual, 77)
+		So(uint16Val.Get(), ShouldEqual, 88)
+		So(uint32Val.Get(), ShouldEqual, 99)
+		So(uint64Val.Get(), ShouldEqual, 1010)
+		So(float32Val.Get(), ShouldBeBetween, 32.31, 32.33)
+		So(float64Val.Get(), ShouldBeBetween, 64.63, 64.65)
+		So(stringVal.Get(), ShouldEqual, "world")
+		So(durationVal.Get(), ShouldEqual, 6*time.Second)
+		So(timeVal.Get(), ShouldEqual, time.Unix(1604930127, 0))
+		So(ipVal.Get(), ShouldEqual, net.ParseIP("106.11.34.43"))
+
+		DeleteTestFile()
+	})
+}
