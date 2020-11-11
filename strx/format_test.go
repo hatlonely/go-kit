e61858251367ea04b9d2eae05d00b046623acc92
hatlonely <hatlonely@gmail.com>
2020-11-11 16:48:54 +0800

    update

---
+++ strx/format_test.go
@@ -0,0 +1,40 @@
+package strx
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestJsonMarshal(t *testing.T) {
+	v := map[string]interface{}{
+		"key1": "val1",
+		"key2": 2,
+		"key3": "val3",
+		"key4": map[string]interface{}{
+			"key5": "val5",
+			"key6": 6,
+			"key7": []int{1, 2, 3},
+			"key8": []map[interface{}]interface{}{
+				{
+					"key9":  9,
+					"key10": "val10",
+				}, {
+					"key11": 11,
+					12:      "val12",
+				},
+			},
+		},
+	}
+	fmt.Println(JsonMarshal(v))
+	fmt.Println(JsonMarshalIndent(v))
+	fmt.Println(JsonMarshalSortKeys(v))
+	fmt.Println(JsonMarshalIndentSortKeys(v))
+}
+
+func TestFormatSpace(t *testing.T) {
+	Convey("TestFormatSpace", t, func() {
+		So(FormatSpace("  hello  \t\n  world \r\n  hello  golang\n"), ShouldEqual, "hello world hello golang")
+	})
+}
