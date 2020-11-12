78e40ef146d4f84d3f78cce0c6560208ee79230f
hatlonely <hatlonely@gmail.com>
2020-11-12 13:54:48 +0800

    update

--- cast/autogen_set_interface_test.go
+++ cast/autogen_set_interface_test.go
@@ -175,17 +175,149 @@ func TestSetInterfaceSlice(t *testing.T) {
 			So(SetInterface(&val, "1s,2ms,3m"), ShouldBeNil)
 			So(val, ShouldResemble, []time.Duration{time.Second, 2 * time.Millisecond, 3 * time.Minute})
 		}
-
 		{
 			var val []time.Time
 			So(SetInterface(&val, "1970-01-01 08:00:01 +0800 CST,1970-01-01T08:00:02+08:00,1970-01-01T08:00:03+08:00"), ShouldBeNil)
 			So(val, ShouldResemble, []time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)})
 		}
-
 		{
 			var val []net.IP
 			So(SetInterface(&val, "192.168.0.1,192.168.0.2"), ShouldBeNil)
 			So(val, ShouldResemble, []net.IP{net.ParseIP("192.168.0.1"), net.ParseIP("192.168.0.2")})
+		}
+	})
+}
+
+func TestSetInterfaceError(t *testing.T) {
+	Convey("TestSetInterfaceError", t, func() {
+		{
+			var val bool
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val int
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val int8
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val int16
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val int32
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val int64
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val uint
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val uint8
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val uint16
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val uint32
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val uint64
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val float32
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val float64
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val time.Duration
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val net.IP
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val time.Time
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+
+		{
+			var val []bool
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []int
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []int8
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []int16
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []int32
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []int64
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []uint
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []uint8
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []uint16
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []uint32
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []uint64
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []float32
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []float64
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []time.Duration
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []time.Time
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
+		}
+		{
+			var val []net.IP
+			So(SetInterface(&val, "x"), ShouldNotBeNil)
 		}
 	})
 }
