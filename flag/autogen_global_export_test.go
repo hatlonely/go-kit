6b2d683d02b1262804abcd95d31aaca09ef499f0
hatlonely <hatlonely@gmail.com>
2020-11-15 14:39:23 +0800

    update

--- flag/autogen_global_export_test.go
+++ flag/autogen_global_export_test.go
@@ -1,4 +1,4 @@
-package flag_test
+package flag
 
 import (
 	"net"
@@ -7,48 +7,48 @@ import (
 	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
-
-	"github.com/hatlonely/go-kit/flag"
 )
 
 func TestGlobalFlagBind(t *testing.T) {
 	Convey("TestGlobalFlag", t, func() {
-		boolVal := flag.Bool("bool-val", false, "")
-		intVal := flag.Int("int-val", 1, "")
-		int8Val := flag.Int8("int8-val", 2, "")
-		int16Val := flag.Int16("int16-val", 3, "")
-		int32Val := flag.Int32("int32-val", 4, "")
-		int64Val := flag.Int64("int64-val", 5, "")
-		uintVal := flag.Uint("uint-val", 6, "")
-		uint8Val := flag.Uint8("uint8-val", 7, "")
-		uint16Val := flag.Uint16("uint16-val", 8, "")
-		uint32Val := flag.Uint32("uint32-val", 9, "")
-		uint64Val := flag.Uint64("uint64-val", 10, "")
-		float32Val := flag.Float32("float32-val", 3.2, "")
-		float64Val := flag.Float64("float64-val", 6.4, "")
-		stringVal := flag.String("string-val", "hello", "")
-		timeVal := flag.Time("time-val", time.Unix(1604930126, 0), "")
-		durationVal := flag.Duration("duration-val", 3*time.Second, "")
-		ipVal := flag.IP("ip-val", net.ParseIP("106.11.34.42"), "")
-		boolSliceVal := flag.BoolSlice("bool-slice-val", []bool{true, true, false}, "")
-		intSliceVal := flag.IntSlice("int-slice-val", []int{1, 2, 3}, "")
-		int8SliceVal := flag.Int8Slice("int8-slice-val", []int8{1, 2, 3}, "")
-		int16SliceVal := flag.Int16Slice("int16-slice-val", []int16{1, 2, 3}, "")
-		int32SliceVal := flag.Int32Slice("int32-slice-val", []int32{1, 2, 3}, "")
-		int64SliceVal := flag.Int64Slice("int64-slice-val", []int64{1, 2, 3}, "")
-		uintSliceVal := flag.UintSlice("uint-slice-val", []uint{1, 2, 3}, "")
-		uint8SliceVal := flag.Uint8Slice("uint8-slice-val", []uint8{1, 2, 3}, "")
-		uint16SliceVal := flag.Uint16Slice("uint16-slice-val", []uint16{1, 2, 3}, "")
-		uint32SliceVal := flag.Uint32Slice("uint32-slice-val", []uint32{1, 2, 3}, "")
-		uint64SliceVal := flag.Uint64Slice("uint64-slice-val", []uint64{1, 2, 3}, "")
-		float32SliceVal := flag.Float32Slice("float32-slice-val", []float32{1.1, 2.2, 3.3}, "")
-		float64SliceVal := flag.Float64Slice("float64-slice-val", []float64{1.1, 2.2, 3.3}, "")
-		stringSliceVal := flag.StringSlice("string-slice-val", []string{"1", "2", "3"}, "")
-		timeSliceVal := flag.TimeSlice("time-slice-val", []time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)}, "")
-		durationSliceVal := flag.DurationSlice("duration-slice-val", []time.Duration{time.Second, 2 * time.Second, 3 * time.Second}, "")
-		ipSliceVal := flag.IPSlice("ip-slice-val", []net.IP{net.ParseIP("192.168.0.1"), net.ParseIP("192.168.0.2")}, "")
+		gflag = NewFlag("test")
 
-		So(flag.ParseArgs(strings.Split("--bool-val "+
+		boolVal := Bool("bool-val", false, "")
+		intVal := Int("int-val", 1, "")
+		int8Val := Int8("int8-val", 2, "")
+		int16Val := Int16("int16-val", 3, "")
+		int32Val := Int32("int32-val", 4, "")
+		int64Val := Int64("int64-val", 5, "")
+		uintVal := Uint("uint-val", 6, "")
+		uint8Val := Uint8("uint8-val", 7, "")
+		uint16Val := Uint16("uint16-val", 8, "")
+		uint32Val := Uint32("uint32-val", 9, "")
+		uint64Val := Uint64("uint64-val", 10, "")
+		float32Val := Float32("float32-val", 3.2, "")
+		float64Val := Float64("float64-val", 6.4, "")
+		stringVal := String("string-val", "hello", "")
+		timeVal := Time("time-val", time.Unix(1604930126, 0), "")
+		durationVal := Duration("duration-val", 3*time.Second, "")
+		ipVal := IP("ip-val", net.ParseIP("106.11.34.42"), "")
+		boolSliceVal := BoolSlice("bool-slice-val", []bool{true, true, false}, "")
+		intSliceVal := IntSlice("int-slice-val", []int{1, 2, 3}, "")
+		int8SliceVal := Int8Slice("int8-slice-val", []int8{1, 2, 3}, "")
+		int16SliceVal := Int16Slice("int16-slice-val", []int16{1, 2, 3}, "")
+		int32SliceVal := Int32Slice("int32-slice-val", []int32{1, 2, 3}, "")
+		int64SliceVal := Int64Slice("int64-slice-val", []int64{1, 2, 3}, "")
+		uintSliceVal := UintSlice("uint-slice-val", []uint{1, 2, 3}, "")
+		uint8SliceVal := Uint8Slice("uint8-slice-val", []uint8{1, 2, 3}, "")
+		uint16SliceVal := Uint16Slice("uint16-slice-val", []uint16{1, 2, 3}, "")
+		uint32SliceVal := Uint32Slice("uint32-slice-val", []uint32{1, 2, 3}, "")
+		uint64SliceVal := Uint64Slice("uint64-slice-val", []uint64{1, 2, 3}, "")
+		float32SliceVal := Float32Slice("float32-slice-val", []float32{1.1, 2.2, 3.3}, "")
+		float64SliceVal := Float64Slice("float64-slice-val", []float64{1.1, 2.2, 3.3}, "")
+		stringSliceVal := StringSlice("string-slice-val", []string{"1", "2", "3"}, "")
+		timeSliceVal := TimeSlice("time-slice-val", []time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)}, "")
+		durationSliceVal := DurationSlice("duration-slice-val", []time.Duration{time.Second, 2 * time.Second, 3 * time.Second}, "")
+		ipSliceVal := IPSlice("ip-slice-val", []net.IP{net.ParseIP("192.168.0.1"), net.ParseIP("192.168.0.2")}, "")
+
+		So(ParseArgs(strings.Split("--bool-val "+
 			"--int-val 11 --int8-val 22 --int16-val 33 --int32-val 44 --int64-val 55 "+
 			"--uint-val 66 --uint8-val 77 --uint16-val 88 --uint32-val 99 --uint64-val 1010 "+
 			"--float32-val 32.32 --float64-val 64.64 --string-val world "+
@@ -99,6 +99,8 @@ func TestGlobalFlagBind(t *testing.T) {
 
 func TestGlobalFlagBindVar(t *testing.T) {
 	Convey("TestGlobalFlag", t, func() {
+		gflag = NewFlag("test")
+
 		var boolVal bool
 		var intVal int
 		var int8Val int8
@@ -134,42 +136,42 @@ func TestGlobalFlagBindVar(t *testing.T) {
 		var durationSliceVal []time.Duration
 		var ipSliceVal []net.IP
 
-		flag.BoolVar(&boolVal, "bool-val", false, "")
-		flag.IntVar(&intVal, "int-val", 1, "")
-		flag.Int8Var(&int8Val, "int8-val", 2, "")
-		flag.Int16Var(&int16Val, "int16-val", 3, "")
-		flag.Int32Var(&int32Val, "int32-val", 4, "")
-		flag.Int64Var(&int64Val, "int64-val", 5, "")
-		flag.UintVar(&uintVal, "uint-val", 6, "")
-		flag.Uint8Var(&uint8Val, "uint8-val", 7, "")
-		flag.Uint16Var(&uint16Val, "uint16-val", 8, "")
-		flag.Uint32Var(&uint32Val, "uint32-val", 9, "")
-		flag.Uint64Var(&uint64Val, "uint64-val", 10, "")
-		flag.Float32Var(&float32Val, "float32-val", 3.2, "")
-		flag.Float64Var(&float64Val, "float64-val", 6.4, "")
-		flag.StringVar(&stringVal, "string-val", "hello", "")
-		flag.TimeVar(&timeVal, "time-val", time.Unix(1604930126, 0), "")
-		flag.DurationVar(&durationVal, "duration-val", 3*time.Second, "")
-		flag.IPVar(&ipVal, "ip-val", net.ParseIP("106.11.34.42"), "")
-		flag.BoolSliceVar(&boolSliceVal, "bool-slice-val", []bool{true, true, false}, "")
-		flag.IntSliceVar(&intSliceVal, "int-slice-val", []int{1, 2, 3}, "")
-		flag.Int8SliceVar(&int8SliceVal, "int8-slice-val", []int8{1, 2, 3}, "")
-		flag.Int16SliceVar(&int16SliceVal, "int16-slice-val", []int16{1, 2, 3}, "")
-		flag.Int32SliceVar(&int32SliceVal, "int32-slice-val", []int32{1, 2, 3}, "")
-		flag.Int64SliceVar(&int64SliceVal, "int64-slice-val", []int64{1, 2, 3}, "")
-		flag.UintSliceVar(&uintSliceVal, "uint-slice-val", []uint{1, 2, 3}, "")
-		flag.Uint8SliceVar(&uint8SliceVal, "uint8-slice-val", []uint8{1, 2, 3}, "")
-		flag.Uint16SliceVar(&uint16SliceVal, "uint16-slice-val", []uint16{1, 2, 3}, "")
-		flag.Uint32SliceVar(&uint32SliceVal, "uint32-slice-val", []uint32{1, 2, 3}, "")
-		flag.Uint64SliceVar(&uint64SliceVal, "uint64-slice-val", []uint64{1, 2, 3}, "")
-		flag.Float32SliceVar(&float32SliceVal, "float32-slice-val", []float32{1.1, 2.2, 3.3}, "")
-		flag.Float64SliceVar(&float64SliceVal, "float64-slice-val", []float64{1.1, 2.2, 3.3}, "")
-		flag.StringSliceVar(&stringSliceVal, "string-slice-val", []string{"1", "2", "3"}, "")
-		flag.TimeSliceVar(&timeSliceVal, "time-slice-val", []time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)}, "")
-		flag.DurationSliceVar(&durationSliceVal, "duration-slice-val", []time.Duration{time.Second, 2 * time.Second, 3 * time.Second}, "")
-		flag.IPSliceVar(&ipSliceVal, "ip-slice-val", []net.IP{net.ParseIP("192.168.0.1"), net.ParseIP("192.168.0.2")}, "")
+		BoolVar(&boolVal, "bool-val", false, "")
+		IntVar(&intVal, "int-val", 1, "")
+		Int8Var(&int8Val, "int8-val", 2, "")
+		Int16Var(&int16Val, "int16-val", 3, "")
+		Int32Var(&int32Val, "int32-val", 4, "")
+		Int64Var(&int64Val, "int64-val", 5, "")
+		UintVar(&uintVal, "uint-val", 6, "")
+		Uint8Var(&uint8Val, "uint8-val", 7, "")
+		Uint16Var(&uint16Val, "uint16-val", 8, "")
+		Uint32Var(&uint32Val, "uint32-val", 9, "")
+		Uint64Var(&uint64Val, "uint64-val", 10, "")
+		Float32Var(&float32Val, "float32-val", 3.2, "")
+		Float64Var(&float64Val, "float64-val", 6.4, "")
+		StringVar(&stringVal, "string-val", "hello", "")
+		TimeVar(&timeVal, "time-val", time.Unix(1604930126, 0), "")
+		DurationVar(&durationVal, "duration-val", 3*time.Second, "")
+		IPVar(&ipVal, "ip-val", net.ParseIP("106.11.34.42"), "")
+		BoolSliceVar(&boolSliceVal, "bool-slice-val", []bool{true, true, false}, "")
+		IntSliceVar(&intSliceVal, "int-slice-val", []int{1, 2, 3}, "")
+		Int8SliceVar(&int8SliceVal, "int8-slice-val", []int8{1, 2, 3}, "")
+		Int16SliceVar(&int16SliceVal, "int16-slice-val", []int16{1, 2, 3}, "")
+		Int32SliceVar(&int32SliceVal, "int32-slice-val", []int32{1, 2, 3}, "")
+		Int64SliceVar(&int64SliceVal, "int64-slice-val", []int64{1, 2, 3}, "")
+		UintSliceVar(&uintSliceVal, "uint-slice-val", []uint{1, 2, 3}, "")
+		Uint8SliceVar(&uint8SliceVal, "uint8-slice-val", []uint8{1, 2, 3}, "")
+		Uint16SliceVar(&uint16SliceVal, "uint16-slice-val", []uint16{1, 2, 3}, "")
+		Uint32SliceVar(&uint32SliceVal, "uint32-slice-val", []uint32{1, 2, 3}, "")
+		Uint64SliceVar(&uint64SliceVal, "uint64-slice-val", []uint64{1, 2, 3}, "")
+		Float32SliceVar(&float32SliceVal, "float32-slice-val", []float32{1.1, 2.2, 3.3}, "")
+		Float64SliceVar(&float64SliceVal, "float64-slice-val", []float64{1.1, 2.2, 3.3}, "")
+		StringSliceVar(&stringSliceVal, "string-slice-val", []string{"1", "2", "3"}, "")
+		TimeSliceVar(&timeSliceVal, "time-slice-val", []time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)}, "")
+		DurationSliceVar(&durationSliceVal, "duration-slice-val", []time.Duration{time.Second, 2 * time.Second, 3 * time.Second}, "")
+		IPSliceVar(&ipSliceVal, "ip-slice-val", []net.IP{net.ParseIP("192.168.0.1"), net.ParseIP("192.168.0.2")}, "")
 
-		So(flag.ParseArgs(strings.Split("--bool-val "+
+		So(ParseArgs(strings.Split("--bool-val "+
 			"--int-val 11 --int8-val 22 --int16-val 33 --int32-val 44 --int64-val 55 "+
 			"--uint-val 66 --uint8-val 77 --uint16-val 88 --uint32-val 99 --uint64-val 1010 "+
 			"--float32-val 32.32 --float64-val 64.64 --string-val world "+
