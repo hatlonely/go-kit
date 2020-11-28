c614628615b86a5a400979492202308fed1c3fda
hatlonely <hatlonely@gmail.com>
2020-11-28 16:39:06 +0800

    update

--- rpcx/mux_test.go
+++ rpcx/mux_test.go
@@ -1,1 +1,19 @@
 package rpcx
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestStatusErrorDetail(t *testing.T) {
+	Convey("TestStatusErrorDetail", t, func() {
+		detail := StatusErrorDetail(errors.New("hello world"), "test-request-id")
+		So(detail.Status, ShouldEqual, http.StatusInternalServerError)
+		So(detail.RequestID, ShouldEqual, "test-request-id")
+		So(detail.Message, ShouldEqual, "hello world")
+		So(detail.Code, ShouldEqual, "InternalError")
+	})
+}
