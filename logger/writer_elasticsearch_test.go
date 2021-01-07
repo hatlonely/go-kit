9c61f246fb36614bf0f99a0f889f9ac9692e4a00
hatlonely <hatlonely@gmail.com>
2021-01-08 00:04:14 +0800

    update

--- logger/writer_elasticsearch_test.go
+++ logger/writer_elasticsearch_test.go
@@ -1,15 +1,28 @@
 package logger
 
 import (
+	"context"
+	"reflect"
 	"testing"
 	"time"
 
+	. "github.com/agiledragon/gomonkey"
+	"github.com/olivere/elastic/v7"
 	. "github.com/smartystreets/goconvey/convey"
 
 	"github.com/hatlonely/go-kit/cli"
 )
 
 func TestElasticSearchWriter_Write(t *testing.T) {
+	patches := ApplyFunc(cli.NewElasticSearchWithOptions, func(options *cli.ElasticSearchOptions) (*elastic.Client, error) {
+		return &elastic.Client{}, nil
+	}).ApplyMethod(reflect.TypeOf(&elastic.IndexService{}), "Do", func(
+		s *elastic.IndexService, ctx context.Context) (*elastic.IndexResponse, error) {
+		return nil, nil
+	})
+
+	defer patches.Reset()
+
 	Convey("", t, func() {
 		writer, err := NewWriterWithOptions(&WriterOptions{
 			Type: "ElasticSearch",
@@ -20,7 +33,7 @@ func TestElasticSearchWriter_Write(t *testing.T) {
 				MsgChanLen: 200,
 				WorkerNum:  2,
 				ElasticSearch: cli.ElasticSearchOptions{
-					URI: "http://127.0.0.1:9200",
+					URI: "http://127.0.0.2:9200",
 				},
 			},
 		})
