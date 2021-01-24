e8a6414b5a162d2145b5f868aee49abe0ddad592
hatlonely <hatlonely@gmail.com>
2021-01-24 17:11:32 +0800

    update

--- wrap/constructor_gorm.go
+++ wrap/constructor_gorm.go
@@ -11,20 +11,22 @@ import (
 	"github.com/hatlonely/go-kit/refx"
 )
 
+type GormOptions struct {
+	Username        string `dft:"root"`
+	Password        string
+	Database        string
+	Host            string        `dft:"localhost"`
+	Port            int           `dft:"3306"`
+	ConnMaxLifeTime time.Duration `dft:"60s"`
+	MaxIdleConns    int           `dft:"10"`
+	MaxOpenConns    int           `dft:"20"`
+	LogMode         bool
+}
+
 type GORMDBWrapperOptions struct {
 	Retry   RetryOptions
 	Wrapper WrapperOptions
-	Gorm    struct {
-		Username        string `dft:"root"`
-		Password        string
-		Database        string
-		Host            string        `dft:"localhost"`
-		Port            int           `dft:"3306"`
-		ConnMaxLifeTime time.Duration `dft:"60s"`
-		MaxIdleConns    int           `dft:"10"`
-		MaxOpenConns    int           `dft:"20"`
-		LogMode         bool
-	}
+	Gorm    GormOptions
 }
 
 func NewGORMDBWrapperWithOptions(options *GORMDBWrapperOptions) (*GORMDBWrapper, error) {
@@ -69,20 +71,49 @@ func NewGORMDBWrapperWithConfig(cfg *config.Config, opts ...refx.Option) (*GORMD
 
 	refxOptions := refx.NewOptions(opts...)
 	cfg.AddOnItemChangeHandler(refxOptions.FormatKey("Wrapper"), func(cfg *config.Config) error {
+		var options WrapperOptions
 		if err := cfg.Unmarshal(&options, opts...); err != nil {
 			return errors.Wrap(err, "cfg.Unmarshal failed")
 		}
+		w.options = &options
+		return nil
+	})
+	cfg.AddOnItemChangeHandler(refxOptions.FormatKey("Retry"), func(cfg *config.Config) error {
+		var options RetryOptions
+		if err := cfg.Unmarshal(&options, opts...); err != nil {
+			return errors.Wrap(err, "cfg.Unmarshal failed")
+		}
+		retry, err := NewRetryWithOptions(&options)
+		if err != nil {
+			return errors.Wrap(err, "NewRetryWithOptions failed")
+		}
+		w.retry = retry
+		return nil
+	})
+	cfg.AddOnItemChangeHandler(refxOptions.FormatKey("Gorm"), func(cfg *config.Config) error {
+		var options GormOptions
+		if err := cfg.Unmarshal(&options, opts...); err != nil {
+			return errors.Wrap(err, "cfg.Unmarshal failed")
+		}
+		cli, err := gorm.Open("mysql", fmt.Sprintf(
+			"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+			options.Username, options.Password, options.Host, options.Port, options.Database,
+		))
+		if err != nil {
+			return errors.Wrap(err, "gorm.Open failed")
+		}
+		if options.ConnMaxLifeTime != 0 {
+			cli.DB().SetConnMaxLifetime(options.ConnMaxLifeTime)
+		}
+		if options.MaxOpenConns != 0 {
+			cli.DB().SetMaxOpenConns(options.MaxOpenConns)
+		}
+		if options.MaxIdleConns != 0 {
+			cli.DB().SetMaxIdleConns(options.MaxIdleConns)
+		}
+		w.obj = cli
 		return nil
 	})
 
 	return w, err
 }
-
-//func (w *GORMDBWrapper) OnChangeHandler(cfg *config.Config, opts ...refx.Option) {
-//	var options GORMDBWrapperOptions
-//	if err := cfg.Unmarshal(&options, opts...); err != nil {
-//		fmt.Println(err)
-//		return
-//	}
-//	c.options = &options
-//}
