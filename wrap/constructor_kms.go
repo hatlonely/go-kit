da8bbc27c25f33069eff24655acb1ffecef65068
hatlonely <hatlonely@gmail.com>
2021-01-24 23:46:39 +0800

    update

--- wrap/constructor_kms.go
+++ wrap/constructor_kms.go
@@ -5,16 +5,20 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/hatlonely/go-kit/alics"
+	"github.com/hatlonely/go-kit/config"
+	"github.com/hatlonely/go-kit/refx"
 )
+
+type KMSOptions struct {
+	RegionID        string
+	AccessKeyID     string
+	AccessKeySecret string
+}
 
 type KMSClientWrapperOptions struct {
 	Retry   RetryOptions
 	Wrapper WrapperOptions
-	KMS     struct {
-		RegionID        string
-		AccessKeyID     string
-		AccessKeySecret string
-	}
+	KMS     KMSOptions
 }
 
 func NewKMSClientWrapperWithOptions(options *KMSClientWrapperOptions) (*KMSClientWrapper, error) {
@@ -27,6 +31,9 @@ func NewKMSClientWrapperWithOptions(options *KMSClientWrapperOptions) (*KMSClien
 		client, err := kms.NewClientWithAccessKey(options.KMS.RegionID, options.KMS.AccessKeyID, options.KMS.AccessKeySecret)
 		if err != nil {
 			return nil, errors.Wrap(err, "kms.NewClientWithAccessKey failed")
+		}
+		if _, err := client.ListKeys(kms.CreateListKeysRequest()); err != nil {
+			return nil, errors.Wrap(err, "kms.Client.ListKeys failed")
 		}
 		return &KMSClientWrapper{
 			obj:     client,
@@ -44,10 +51,63 @@ func NewKMSClientWrapperWithOptions(options *KMSClientWrapperOptions) (*KMSClien
 	if err != nil {
 		return nil, errors.Wrap(err, "kms.NewClientWithEcsRamRole failed")
 	}
+	if _, err := client.ListKeys(kms.CreateListKeysRequest()); err != nil {
+		return nil, errors.Wrap(err, "kms.Client.ListKeys failed")
+	}
 
 	return &KMSClientWrapper{
 		obj:     client,
 		retry:   retry,
 		options: &options.Wrapper,
 	}, nil
+}
+
+func NewKMSClientWrapperWithConfig(cfg *config.Config, opts ...refx.Option) (*KMSClientWrapper, error) {
+	var options KMSClientWrapperOptions
+	if err := cfg.Unmarshal(&options, opts...); err != nil {
+		return nil, err
+	}
+	w, err := NewKMSClientWrapperWithOptions(&options)
+	if err != nil {
+		return nil, err
+	}
+
+	refxOptions := refx.NewOptions(opts...)
+	cfg.AddOnItemChangeHandler(refxOptions.FormatKey("Wrapper"), w.OnWrapperChange(opts...))
+	cfg.AddOnItemChangeHandler(refxOptions.FormatKey("Retry"), w.OnRetryChange(opts...))
+	cfg.AddOnItemChangeHandler(refxOptions.FormatKey("Gorm"), func(cfg *config.Config) error {
+		var options KMSOptions
+		if err := cfg.Unmarshal(&options, opts...); err != nil {
+			return errors.Wrap(err, "cfg.Unmarshal failed")
+		}
+
+		if options.AccessKeyID != "" {
+			client, err := kms.NewClientWithAccessKey(options.RegionID, options.AccessKeyID, options.AccessKeySecret)
+			if err != nil {
+				return errors.Wrap(err, "kms.NewClientWithAccessKey failed")
+			}
+			if _, err := client.ListKeys(kms.CreateListKeysRequest()); err != nil {
+				return errors.Wrap(err, "kms.Client.ListKeys failed")
+			}
+			w.obj = client
+			return nil
+		}
+
+		role, err := alics.ECSMetaDataRamSecurityCredentialsRole()
+		if err != nil {
+			return errors.Wrap(err, "alics.ECSMetaDataRamSecurityCredentialsRole failed")
+		}
+		client, err := kms.NewClientWithEcsRamRole(options.RegionID, role)
+		if err != nil {
+			return errors.Wrap(err, "kms.NewClientWithEcsRamRole failed")
+		}
+		if _, err := client.ListKeys(kms.CreateListKeysRequest()); err != nil {
+			return errors.Wrap(err, "kms.Client.ListKeys failed")
+		}
+		w.obj = client
+
+		return nil
+	})
+
+	return w, err
 }
