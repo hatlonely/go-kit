a0ba08c47ee8ae28a07b1c6359ddc0c2bc46f054
hatlonely <hatlonely@gmail.com>
2021-01-29 22:37:50 +0800

    update

--- wrap/ratelimiter.go
+++ wrap/ratelimiter.go
@@ -11,77 +11,36 @@ import (
 )
 
 func RegisterRateLimiterGroup(key string, constructor interface{}) {
-	if _, ok := rateLimiterGroupMap[key]; ok {
+	if _, ok := rateLimiterGroupConstructorMap[key]; ok {
 		panic(fmt.Sprintf("ratelimiter type [%v] is already registered", key))
 	}
 
-	rt := reflect.TypeOf(constructor)
-	if rt.Kind() != reflect.Func {
-		panic("constructor should be a function type")
-	}
-	if rt.NumIn() > 1 {
-		panic("constructor parameters number should not greater than 1")
-	}
-	if rt.NumOut() > 2 {
-		panic("constructor return number should not greater than 2")
-	}
-	if rt.NumOut() == 0 || !rt.Out(0).Implements(reflect.TypeOf((*RateLimiterGroup)(nil)).Elem()) {
-		panic("constructor should return a RateLimiterGroup")
-	}
-	if rt.NumOut() == 2 && rt.Out(1) != reflect.TypeOf((*error)(nil)).Elem() {
-		panic("constructor should return an error")
-	}
+	info, err := refx.NewConstructor(constructor, reflect.TypeOf((*RateLimiterGroup)(nil)).Elem())
+	refx.Must(err)
 
-	info := &RateLimiterGroupConstructor{
-		HasParam:    rt.NumIn() == 1,
-		ReturnError: rt.NumOut() == 2,
-		FuncValue:   reflect.ValueOf(constructor),
-	}
-
-	if rt.NumIn() > 0 {
-		info.ParamType = rt.In(0)
-	}
-
-	rateLimiterGroupMap[key] = info
+	rateLimiterGroupConstructorMap[key] = info
 }
 
-type RateLimiterGroupConstructor struct {
-	HasParam    bool
-	ReturnError bool
-	ParamType   reflect.Type
-	FuncValue   reflect.Value
-}
-
-var rateLimiterGroupMap = map[string]*RateLimiterGroupConstructor{}
+var rateLimiterGroupConstructorMap = map[string]*refx.Constructor{}
 
 type RateLimiterGroup interface {
 	Wait(ctx context.Context, key string) error
 }
 
 func NewRateLimiterGroupWithOptions(options *RateLimiterGroupOptions, opts ...refx.Option) (RateLimiterGroup, error) {
-	constructor, ok := rateLimiterGroupMap[options.Type]
+	constructor, ok := rateLimiterGroupConstructorMap[options.Type]
 	if !ok {
-		return nil, errors.Errorf("unsupported rate limiter type: [%v]", options.Type)
+		return nil, errors.Errorf("unregistered decoder type: [%v]", options.Type)
 	}
 
-	var result []reflect.Value
-	if constructor.HasParam {
-		if reflect.TypeOf(options.Options) == constructor.ParamType {
-			result = constructor.FuncValue.Call([]reflect.Value{reflect.ValueOf(options.Options)})
-		} else {
-			params := reflect.New(constructor.ParamType)
-			if err := refx.InterfaceToStruct(options.Options, params.Interface(), opts...); err != nil {
-				return nil, errors.Wrap(err, "refx.InterfaceToStruct failed")
-			}
-			result = constructor.FuncValue.Call([]reflect.Value{params.Elem()})
-		}
-	} else {
-		result = constructor.FuncValue.Call(nil)
+	result, err := constructor.Call(options.Options, opts...)
+	if err != nil {
+		return nil, errors.WithMessage(err, "constructor.Call failed")
 	}
 
 	if constructor.ReturnError {
 		if !result[1].IsNil() {
-			return nil, result[1].Interface().(error)
+			return nil, errors.Wrapf(result[1].Interface().(error), "NewDecoder failed. type: [%v]", options.Type)
 		}
 		return result[0].Interface().(RateLimiterGroup), nil
 	}
