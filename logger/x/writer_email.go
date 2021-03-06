afcf8d9d9826e9bd4010ae2b4a7ee1611d23f1f7
hatlonely <hatlonely@gmail.com>
2021-03-06 17:34:50 +0800

    update

---
+++ logger/x/writer_email.go
@@ -0,0 +1,102 @@
+package loggerx
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/smtp"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+
+	"github.com/hatlonely/go-kit/logger"
+	"github.com/hatlonely/go-kit/strx"
+)
+
+type EmailWriterOptions struct {
+	Server   string
+	Port     int
+	Password string
+	Username string
+
+	ToList  []string
+	Subject string
+
+	MsgChanLen int `dft:"200"`
+	WorkerNum  int `dft:"1"`
+
+	Formatter logger.FormatterOptions
+}
+
+func NewEmailWriterWithOptions(options *EmailWriterOptions) (*EmailWriter, error) {
+	formatter, err := logger.NewFormatterWithOptions(&options.Formatter)
+	if err != nil {
+		return nil, errors.WithMessage(err, "logger.NewFormatterWithOptions failed")
+	}
+
+	w := &EmailWriter{
+		options:   options,
+		messages:  make(chan *logger.Info, options.MsgChanLen),
+		formatter: formatter,
+	}
+
+	for i := 0; i < options.WorkerNum; i++ {
+		w.wg.Add(1)
+		go func() {
+			w.work()
+			w.wg.Done()
+		}()
+	}
+
+	return w, nil
+}
+
+type EmailWriter struct {
+	options   *EmailWriterOptions
+	formatter logger.Formatter
+
+	messages chan *logger.Info
+	wg       sync.WaitGroup
+}
+
+func (w *EmailWriter) Write(info *logger.Info) error {
+	w.messages <- info
+	return nil
+}
+
+func (w *EmailWriter) Close() error {
+	close(w.messages)
+	w.wg.Wait()
+
+	return nil
+}
+
+func (w *EmailWriter) work() {
+	for info := range w.messages {
+		buf, err := w.formatter.Format(info)
+		if err != nil {
+			buf, _ = json.MarshalIndent(info, "  ", "  ")
+		}
+
+		if err := w.send(w.options.ToList, w.options.Subject, string(buf)); err != nil {
+			fmt.Printf("EmailWriter write log failed. err: [%+v], kvs: [%v]\n", err, strx.JsonMarshal(info))
+		}
+	}
+}
+
+func (w *EmailWriter) send(toList []string, subject string, body string) error {
+	content := fmt.Sprintf(`From: %v
+To: %v
+Subject: %v
+Content-Type: text/html; charset=UTF-8;
+%v
+`, w.options.Username, strings.Join(toList, ";"), subject, body)
+
+	return smtp.SendMail(
+		fmt.Sprintf("%v:%v", w.options.Server, w.options.Port),
+		smtp.PlainAuth("", w.options.Username, w.options.Password, w.options.Server),
+		w.options.Username,
+		toList,
+		[]byte(content),
+	)
+}
---
+++ logger/x/writer_email_test.go
@@ -0,0 +1,67 @@
+package loggerx
+
+import (
+	"net/smtp"
+	"testing"
+	"time"
+
+	"github.com/hatlonely/go-kit/logger"
+
+	. "github.com/agiledragon/gomonkey"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestEmailWriter_Write(t *testing.T) {
+	ts, _ := time.Parse(time.RFC3339Nano, "2020-11-07T16:07:36.65798+08:00")
+	info := &logger.Info{
+		Time:   ts,
+		Level:  logger.LevelInfo,
+		File:   "formatter_test.go:18",
+		Caller: "TextFormatter",
+		Fields: map[string]interface{}{
+			"meta1": "val1",
+			"meta2": "val2",
+		},
+		Data: map[string]interface{}{
+			"key1": "val1",
+			"key2": "val2",
+		},
+	}
+
+	Convey("TestEmailWriter_Write", t, func(c C) {
+		patches := ApplyFunc(smtp.SendMail, func(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
+			c.So(addr, ShouldEqual, "smtp.qq.com:25")
+			c.So(a, ShouldResemble, smtp.PlainAuth("", "hatlonely@foxmail.com", "xx", "smtp.qq.com"))
+			c.So(from, ShouldEqual, "hatlonely@foxmail.com")
+			c.So(to, ShouldResemble, []string{"hatlonely@foxmail.com"})
+			c.So(string(msg), ShouldStartWith, `From: hatlonely@foxmail.com
+To: hatlonely@foxmail.com
+Subject: 告警测试
+Content-Type: text/html; charset=UTF-8;
+`)
+			return nil
+		})
+		defer patches.Reset()
+
+		w, err := NewEmailWriterWithOptions(&EmailWriterOptions{
+			Server:     "smtp.qq.com",
+			Port:       25,
+			Username:   "hatlonely@foxmail.com",
+			Password:   "xx",
+			ToList:     []string{"hatlonely@foxmail.com"},
+			Subject:    "告警测试",
+			WorkerNum:  1,
+			MsgChanLen: 200,
+			Formatter: logger.FormatterOptions{
+				Type: "Markdown",
+				Options: &MarkdownFormatterOptions{
+					DefaultTitle: "测试告警",
+				},
+			},
+		})
+		So(err, ShouldBeNil)
+
+		So(w.Write(info), ShouldBeNil)
+		So(w.Close(), ShouldBeNil)
+	})
+}
