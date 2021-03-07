c57174e4e9b1602f082f36381ccac2c9eb17ba96
hatlonely <hatlonely@gmail.com>
2021-03-08 00:21:30 +0800

    update

--- logger/x/writer_mns.go
+++ logger/x/writer_mns.go
@@ -1,23 +1,114 @@
 package loggerx
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	ali_mns "github.com/aliyun/aliyun-mns-go-sdk"
+	"github.com/pkg/errors"
+
 	"github.com/hatlonely/go-kit/logger"
+	"github.com/hatlonely/go-kit/strx"
+	"github.com/hatlonely/go-kit/wrap"
 )
 
 type MNSWriterOptions struct {
 	Level      string
 	MsgChanLen int `dft:"200"`
 	WorkerNum  int `dft:"1"`
+	Formatter  logger.FormatterOptions
+	MNS        wrap.MNSClientWrapperOptions
+	Topic      string
+	Timeout    time.Duration `dft:"3s"`
 }
 
-func NewMNSWriterWithOptions(options *MNSWriterOptions) *MNSWriter {
-	return &MNSWriter{}
+func NewMNSWriterWithOptions(options *MNSWriterOptions) (*MNSWriter, error) {
+	level, err := logger.LevelString(options.Level)
+	if err != nil {
+		return nil, errors.Wrap(err, "LevelToString failed")
+	}
+
+	formatter, err := logger.NewFormatterWithOptions(&options.Formatter)
+	if err != nil {
+		return nil, errors.WithMessage(err, "logger.NewFormatterWithOptions failed")
+	}
+
+	client, err := wrap.NewMNSClientWrapperWithOptions(&options.MNS)
+	if err != nil {
+		return nil, errors.WithMessage(err, "wrap.NewMNSClientWrapperWithOptions failed")
+	}
+
+	topic := wrap.NewMNSTopic(options.Topic, client)
+
+	w := &MNSWriter{
+		options:   options,
+		level:     level,
+		formatter: formatter,
+		topic:     topic,
+		messages:  make(chan *logger.Info, options.MsgChanLen),
+	}
+
+	for i := 0; i < options.WorkerNum; i++ {
+		w.wg.Add(1)
+		go func() {
+			w.work()
+			w.wg.Done()
+		}()
+	}
+
+	return w, nil
 }
 
 type MNSWriter struct {
-	options *MNSWriterOptions
+	options   *MNSWriterOptions
+	level     logger.Level
+	formatter logger.Formatter
+
+	messages chan *logger.Info
+	wg       sync.WaitGroup
+
+	topic *wrap.MNSTopicWrapper
+}
+
+func (w *MNSWriter) Write(info *logger.Info) error {
+	if info.Level < w.level {
+		return nil
+	}
+
+	w.messages <- info
+	return nil
+}
+
+func (w *MNSWriter) Close() error {
+	close(w.messages)
+	w.wg.Wait()
+
+	return nil
+}
+
+func (w *MNSWriter) work() {
+	for info := range w.messages {
+		if err := w.send(info); err != nil {
+			fmt.Printf("MNSWriter write log failed. err: [%+v], kvs: [%v]\n", err, strx.JsonMarshal(info))
+		}
+	}
 }
 
 func (w *MNSWriter) send(info *logger.Info) error {
+	buf, err := w.formatter.Format(info)
+	if err != nil {
+		buf, _ = json.MarshalIndent(info, "", "  ")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), w.options.Timeout)
+	defer cancel()
+	_, err = w.topic.PublishMessage(ctx, ali_mns.MessagePublishRequest{
+		MessageBody: string(buf),
+	})
+	if err != nil {
+		return errors.Wrap(err, "topic.PublishMessage failed")
+	}
 	return nil
 }
--- logger/x/writer_mns_test.go
+++ logger/x/writer_mns_test.go
@@ -1,1 +1,65 @@
 package loggerx
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	. "github.com/agiledragon/gomonkey"
+	ali_mns "github.com/aliyun/aliyun-mns-go-sdk"
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/hatlonely/go-kit/logger"
+	"github.com/hatlonely/go-kit/wrap"
+)
+
+func TestMNSWriter_Write(t *testing.T) {
+	ts, _ := time.Parse(time.RFC3339Nano, "2020-11-07T16:07:36.65798+08:00")
+	info := &logger.Info{
+		Time:   ts,
+		Level:  logger.LevelInfo,
+		File:   "formatter_test.go:18",
+		Caller: "TextFormatter",
+		Fields: map[string]interface{}{
+			"meta1": "val1",
+			"meta2": "val2",
+		},
+		Data: map[string]interface{}{
+			"key1": "val1",
+			"key2": "val2",
+		},
+	}
+
+	Convey("TestEmailWriter_Write", t, func(c C) {
+		patches := ApplyMethod(reflect.TypeOf(&ali_mns.MNSTopic{}), "PublishMessage", func(
+			topic *ali_mns.MNSTopic, message ali_mns.MessagePublishRequest) (resp ali_mns.MessageSendResponse, err error) {
+			return ali_mns.MessageSendResponse{}, nil
+		})
+		defer patches.Reset()
+
+		w, err := NewMNSWriterWithOptions(&MNSWriterOptions{
+			Level:      "Info",
+			WorkerNum:  1,
+			MsgChanLen: 200,
+			Formatter: logger.FormatterOptions{
+				Type: "Html",
+				Options: &HtmlFormatterOptions{
+					DefaultTitle: "测试告警",
+				},
+			},
+			MNS: wrap.MNSClientWrapperOptions{
+				MNS: wrap.MNSOptions{
+					Endpoint:        "http://xx.mns.cn-shanghai.aliyuncs.com/",
+					AccessKeyID:     "xx",
+					AccessKeySecret: "xx",
+				},
+			},
+			Topic:   "xx",
+			Timeout: 3 * time.Second,
+		})
+		So(err, ShouldBeNil)
+
+		So(w.Write(info), ShouldBeNil)
+		So(w.Close(), ShouldBeNil)
+	})
+}
