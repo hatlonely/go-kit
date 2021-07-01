ede82d1da8ccdcf48fef1cc6dbebee4c77c006e8
hatlonely <hatlonely@gmail.com>
2021-07-02 00:59:46 +0800

    update

--- logger/writer.go
+++ logger/writer.go
@@ -1,7 +1,6 @@
 package logger
 
 import (
-	"fmt"
 	"reflect"
 
 	"github.com/pkg/errors"
@@ -10,52 +9,25 @@ import (
 	"github.com/hatlonely/go-kit/refx"
 )
 
-func RegisterWriter(key string, constructor interface{}) {
-	if _, ok := writerConstructorMap[key]; ok {
-		panic(fmt.Sprintf("writer type [%v] is already registered", key))
-	}
-
-	info, err := refx.NewConstructor(constructor, reflect.TypeOf((*Writer)(nil)).Elem())
-	refx.Must(err)
-
-	writerConstructorMap[key] = info
-}
-
-var writerConstructorMap = map[string]*refx.Constructor{}
-
 type Writer interface {
 	Write(info *Info) error
 	Close() error
 }
 
-type WriterOptions struct {
-	Type    string
-	Options interface{}
+func RegisterWriter(key string, constructor interface{}) {
+	refx.Register(reflect.TypeOf((*Writer)(nil)).Elem(), key, constructor)
 }
 
-func NewWriterWithOptions(options *WriterOptions, opts ...refx.Option) (Writer, error) {
-	constructor, ok := writerConstructorMap[options.Type]
-	if !ok {
-		return nil, errors.Errorf("unregistered writer type: [%v]", options.Type)
-	}
-
-	result, err := constructor.Call(options.Options, opts...)
+func NewWriterWithOptions(options *refx.TypeOptions, opts ...refx.Option) (Writer, error) {
+	v, err := refx.New(reflect.TypeOf((*Writer)(nil)).Elem(), options, opts...)
 	if err != nil {
-		return nil, errors.WithMessage(err, "constructor.Call failed")
+		return nil, errors.WithMessage(err, "refx.New failed")
 	}
-
-	if constructor.ReturnError {
-		if !result[1].IsNil() {
-			return nil, errors.Wrapf(result[1].Interface().(error), "NewWriter failed. type: [%v]", options.Type)
-		}
-		return result[0].Interface().(Writer), nil
-	}
-
-	return result[0].Interface().(Writer), nil
+	return v.(Writer), nil
 }
 
 func NewWriterWithConfig(cfg *config.Config, opts ...refx.Option) (Writer, error) {
-	var options WriterOptions
+	var options refx.TypeOptions
 	if err := cfg.Unmarshal(&options, opts...); err != nil {
 		return nil, errors.Wrap(err, "cfg.Unmarshal failed.")
 	}
